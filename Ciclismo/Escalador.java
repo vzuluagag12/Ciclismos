552628ec2eb7aab5e3d626fd29d21910ce3fa61f
vzuluagag12 <144352922+vzuluagag12@users.noreply.github.com>
2024-03-10 23:17:27 -0500

    Escalador.java

--- Ciclismo/Escalador.java
+++ Ciclismo/Escalador.java
@@ -10,17 +10,13 @@ public class Escalador extends Ciclista {
         this.gradoRampaSoportada = gradoRampaSoportada;
     }
 
-    // Getters y setters para aceleracionPromedio y gradoRampaSoportada
-
     @Override
     public String imprimirTipo() {
         return "Escalador (Aceleración promedio: " + aceleracionPromedio + ", Grado de rampa soportada: " + gradoRampaSoportada + ")";
     }
 
-    // Método para actualizar el tiempo acumulado
     @Override
     public void registrarTiempo(int tiempo) {
-        // Actualizar el tiempo acumulado con el tiempo registrado
         tiempoAcumulado += tiempo;
     }
 }
