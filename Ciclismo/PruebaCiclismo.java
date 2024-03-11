e0a6e3e352efd92ad57290ef0dbefae3e3bc4e06
vzuluagag12 <144352922+vzuluagag12@users.noreply.github.com>
2024-03-10 23:16:24 -0500

    PruebaCiclismo.java

--- Ciclismo/PruebaCiclismo.java
+++ Ciclismo/PruebaCiclismo.java
@@ -6,22 +6,18 @@ public class PruebaCiclismo {
     public static void main(String[] args) {
         Equipo equipo = new Equipo("Aztra", "Colombia");
 
-        // Creación de instancias de ciclistas con los constructores adecuados
         Velocista ciclistaVelocista = new Velocista(1, "Juan", 100.0, 30.0);
         Escalador ciclistaEscalador = new Escalador(2, "Pedro", 4.5f, 20.0f);
         Contrarrelojista ciclistaContrarrelojista = new Contrarrelojista(3, "María", 40.0);
-
-        // Agregar ciclistas al equipo
+  
         equipo.agregarCiclista(ciclistaVelocista);
         equipo.agregarCiclista(ciclistaEscalador);
         equipo.agregarCiclista(ciclistaContrarrelojista);
 
-        // Mostrar nombres de los ciclistas del equipo
         equipo.listarNombresCiclistas();
-        
-        // Calcular tiempo total del equipo
-        equipo.calcularTotalTiempos(); // Actualizar el tiempo total del equipo
-        int tiempoTotal = Equipo.getTiempoTotal(); // Obtener el tiempo total del equipo
+
+        equipo.calcularTotalTiempos(); 
+        int tiempoTotal = Equipo.getTiempoTotal();
         System.out.println("Tiempo total del equipo: " + tiempoTotal + " minutos");
 
         // Búsqueda de un ciclista por identificador
@@ -30,6 +26,6 @@ public class PruebaCiclismo {
         int idBusqueda = scanner.nextInt();
         equipo.buscarCiclistaPorIdentificador(idBusqueda);
 
-        scanner.close(); // Cerrar el Scanner al finalizar su uso
+        scanner.close(); 
     }
 }
