752cf36cb3ca8a00103e6147c3a850f6e48c02ab
vzuluagag12 <144352922+vzuluagag12@users.noreply.github.com>
2024-03-10 23:18:02 -0500

    Contrarrelojista.java

--- Ciclismo/Contrarrelojista.java
+++ Ciclismo/Contrarrelojista.java
@@ -8,17 +8,13 @@ public class Contrarrelojista extends Ciclista {
         this.velocidadMaxima = velocidadMaxima;
     }
 
-    // Getters y setters para velocidadMaxima
-
     @Override
     public String imprimirTipo() {
         return "Contrarrelojista (Velocidad máxima: " + velocidadMaxima + ")";
     }
 
-    // Método para actualizar el tiempo acumulado
     @Override
     public void registrarTiempo(int tiempo) {
-        // Actualizar el tiempo acumulado con el tiempo registrado
         tiempoAcumulado += tiempo;
     }
 }
